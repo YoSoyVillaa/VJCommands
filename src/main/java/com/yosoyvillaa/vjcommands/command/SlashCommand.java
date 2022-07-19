18a3ccc40e2ee59beea58014abdf956c5745bce4
YoSoyVillaa <contacto@yosoyvillaa.com>
2022-07-19 17:40:00 +0200

    Much better

--- src/main/java/com/yosoyvillaa/vjcommands/command/SlashCommand.java
+++ src/main/java/com/yosoyvillaa/vjcommands/command/SlashCommand.java
@@ -2,12 +2,15 @@ package com.yosoyvillaa.vjcommands.command;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 
 public interface SlashCommand extends ICommand {
 
     String getDescription();
 
-    CommandData getCommandData();
-
     void execute(SlashCommandInteractionEvent event);
+
+    default CommandData getCommandData() {
+        return new CommandDataImpl(getName(), getDescription());
+    }
 }
