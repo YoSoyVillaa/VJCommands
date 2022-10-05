df043e9eb489a45c40c7e001377926d29842736b
YoSoyVillaa <contacto@yosoyvillaa.com>
2022-10-05 11:57:49 +0200

    refactor: migrate to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven("https://jitpack.io")
+    maven("https://repo.maven.apache.org/maven2/")
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-alpha.15")
+}
+
+group = "com.yosoyvillaa"
+version = "1.2"
+description = "VJCommands"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
