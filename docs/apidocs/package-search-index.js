f4a718f8f075efa05a5e2adb79f1936f2c46550c
YoSoyVillaa <contacto@yosoyvillaa.com>
2023-05-29 20:46:15 +0200

    docs: update docs to 1.6

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.yosoyvillaa.vjcommands"},{"l":"com.yosoyvillaa.vjcommands.command"}];updateSearchResults();
---
+++ docs/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.yosoyvillaa.vjcommands","l":"CommandManager"},{"p":"com.yosoyvillaa.vjcommands.command","l":"ICommand"},{"p":"com.yosoyvillaa.vjcommands.command","l":"PrefixCommand"},{"p":"com.yosoyvillaa.vjcommands.command","l":"SlashCommand"}];updateSearchResults();
